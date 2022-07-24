32bed6bda3bfaf72150af4a296baee94d7bdcee1
choiKi <86941191+choiKi@users.noreply.github.com>
2022-07-24 18:12:08 +0900

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by 최기훈 on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
